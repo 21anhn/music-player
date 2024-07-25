28240515b8583f370faacb72d6011555a5b082a9
voleducanh93 <voleducanh93@gmail.com>
2024-07-25 16:08:56 +0700

    BE-register

--- MusicPlayer.DAL/Repositories/UserRepository.cs
+++ MusicPlayer.DAL/Repositories/UserRepository.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using MusicPlayer.DAL.Models;
+using System;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace MusicPlayer.DAL.Repositories
 {
     public class UserRepository
     {
+        private readonly MusicPlayerContext _context;
+
+        public UserRepository(MusicPlayerContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> RegisterUserAsync(User user)
+        {
+            if (user == null)
+                throw new ArgumentNullException(nameof(user));
+
+            if (string.IsNullOrEmpty(user.Username) || string.IsNullOrEmpty(user.Password))
+                return false;
+
+         
+            var existingUser = await _context.Users
+                .FirstOrDefaultAsync(u => u.Username == user.Username);
+
+            if (existingUser != null)
+                return false;
+
+        
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
     }
 }
--- MusicPlayer.PresentationLayer/RegisterWindow.xaml.cs
+++ MusicPlayer.PresentationLayer/RegisterWindow.xaml.cs
@@ -1,25 +1,76 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
+using MusicPlayer.DAL;
+using MusicPlayer.DAL.Models;
+using MusicPlayer.DAL.Repositories;
 
 namespace MusicPlayer.PresentationLayer
 {
     public partial class RegisterWindow : Window
     {
+        private readonly UserRepository _userRepository;
+
         public RegisterWindow()
         {
             InitializeComponent();
+            _userRepository = new UserRepository(new MusicPlayerContext());
         }
 
-        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        private async void RegisterButton_Click(object sender, RoutedEventArgs e)
         {
+            var fullName = FullNameTextBox.Text;
+            var username = EmailTextBox.Text;
+            var password = PasswordTextBox.Password;
+            var confirmPassword = ConfirmPasswordTextBox.Password;
 
+         
+            if (string.IsNullOrEmpty(fullName) || string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please fill in all fields.");
+                return;
+            }
+
+            if (password != confirmPassword)
+            {
+                MessageBox.Show("Passwords do not match.");
+                return;
+            }
+
+            var user = new User
+            {
+                FullName = fullName,
+                Username = username,
+                Password = password
+            };
+
+            try
+            {
+                bool success = await _userRepository.RegisterUserAsync(user);
+
+                if (success)
+                {
+                    MessageBox.Show("Registration successful!");
+                    LoginWindow loginWindow = new LoginWindow();
+                    loginWindow.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Registration failed. The username might be taken.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
         }
 
         private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
-
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.Show();
+            this.Close();
         }
-
-      
     }
 }
