0ccf2bb3712a63e99af9fe82c53077a2df997280
voleducanh93 <voleducanh93@gmail.com>
2024-07-25 14:24:41 +0700

    RegisterWindow

---
+++ MusicPlayer.PresentationLayer/RegisterWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MusicPlayer.PresentationLayer
+{
+    public partial class RegisterWindow : Window
+    {
+        public RegisterWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+      
+    }
+}
